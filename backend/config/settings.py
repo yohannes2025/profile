faddc7e19c0eb4253ea88829daed2b9cf2c0e871
Yohannes M. Tekle <johannes.m.tekle@email.com>
2026-05-31 19:15:55 +0200

    Configure WhiteNoise for production static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
