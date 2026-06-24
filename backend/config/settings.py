063377947b707f7ea655db5d17bca200375540ee
yohannes2025 <185436086+yohannes2025@users.noreply.github.com>
2026-06-25 00:06:24 +0200

    Update Email password in settings.py to solve test error in Github

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -226,7 +226,7 @@ EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="apikey")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="test_password")
 
 DEFAULT_FROM_EMAIL = config(
     "DEFAULT_FROM_EMAIL",
